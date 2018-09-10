d118b7712bc612fe02a3b041e312604adb770701
Jordan Svoboda <jordans2@iastate.edu>
2018-09-10 16:23:53 -0500

    Dataset_Image_Crop is now ready to test and use on a set of image files.

--- Dataset_Image_Crop.py
+++ Dataset_Image_Crop.py
@@ -1,58 +1,81 @@
+#Iterates through the given directory and identifies all faces in images,
+#then crops them and scales them to 244x244 and saves them so that they 
+#may be used for training our model.
 
-#
-#WIP
-#
-
+import os
 import cv2
 from PIL import Image
 import numpy as np
 
 #Set image and cascade file paths
-imagePath = "./Test Images/Mark_Hamill.jpg"
+directory = "./Test Images"
 cascadePath = "./Other Files/haarcascade_frontalface_default.xml"
 
-#Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascadePath)
+i = 1
+for filename in os.listdir(directory):
+    #Only use .jpg and .png files
+    if filename.endswith(".png") or filename.endswith(".jpg"):
+        #Set image path to be the path to the image file
+        imagePath = directory +'/' + filename
+    else:
+        continue    
 
-#Read image
-image = cv2.imread(imagePath)
+    #Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascadePath)
 
-#Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#Detect faces in image
-faces = faceCascade.detectMultiScale(
-    gray,
-    #image,
-    scaleFactor = 1.3,
-    minNeighbors = 5,
-    minSize = (30, 30),
-)
+    #Read image
+    image = cv2.imread(imagePath)
 
-print('Found {0} faces!'.format(len(faces)))
+    #Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #Detect faces in image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        #image,
+        scaleFactor = 1.3,
+        minNeighbors = 5,
+        minSize = (30, 30),
+    )
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    #print('Found {0} faces!'.format(len(faces)))
 
-#Displays the image with the face highlighted.
-#Just to check the detection
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-#Crop image and scale to 224x224
+    #Displays the image with the face highlighted.
+    #Just to check the detection
+    #cv2.imshow("Faces found", image)
+    #cv2.waitKey(0)
 
-image = Image.open(imagePath)
+    #Crop image and scale to 224x224
 
-(x, y, w, h) = faces[0]
+    image = Image.open(imagePath)
 
-center_x = (x + w) / 2
-center_y = (y + h) / 2
+    (x, y, w, h) = faces[0]
 
-box = (x, y, x + w, y + h)
+    center_x = (x + w) / 2
+    center_y = (y + h) / 2
 
-#Crop Image
-cropImage = image.crop(box).resize((224,224))
-cropImage_cv = np.array(cropImage)
-cropImage_cv = cropImage_cv[:, :, ::-1].copy()
-cv2.imshow("Cropped Image", cropImage_cv)
-cv2.waitKey(0)
+    box = (x, y, x + w, y + h)
+
+    #Crop Image
+    cropImage = image.crop(box).resize((224,224))
+
+    #Convert cropped image to an array so it can be read by imshow
+    cropImage_cv = np.array(cropImage)
+
+    #Convert color to RGB
+    cropImage_cv = cv2.cvtColor(cropImage_cv, cv2.COLOR_BGR2RGB)
+    
+    #Display the cropped image until a user presses a key in
+    #the image window
+    #Should only be used to confirm script is running properly
+    cv2.imshow("Cropped Image", cropImage_cv)
+    cv2.waitKey(0)
+
+    #Save the cropped image
+    saveDirectory = "./Dataset/train/Jordan Svoboda"
+    saveName = saveDirectory + "/" + "Jordan_Svoboda" + str(i) + ".jpg"
+    cv2.imwrite(saveName, cropImage_cv)
+    i += 1
