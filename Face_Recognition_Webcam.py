015160b0a056d46c11ce5c814d59abe640a9c3df
Jordan Svoboda <jordans2@iastate.edu>
2018-09-13 11:32:51 -0500

    Created a script, Face_Recognition_Webcam.py, that will allow image capture from webcam.

---
+++ Face_Recognition_Webcam.py
@@ -0,0 +1,42 @@
+#A Script to allow the usage of the webcam to identify faces 
+
+import cv2
+import sys
+
+cascPath = "./Other Files/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+count = 0
+while True:
+    #Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (30,30),
+        #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+#Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('p'):
+        cv2.imwrite("./Test Images/Test Webcam/frame%d.jpg" % count, frame)
+        count += 1
+
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+
+#When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
