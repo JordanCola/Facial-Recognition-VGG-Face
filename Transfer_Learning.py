fdcc79c0905fb3fa4ec9f4e56dbf32211ab12cb7
Jordan Svoboda <jordans2@iastate.edu>
2018-09-20 12:28:58 -0500

    Rewriting Transfer_Learning.py as previous code gave prediction probability of only 33%

--- Transfer_Learning.py
+++ Transfer_Learning.py
@@ -14,13 +14,14 @@ from keras import optimizers
 from PIL import Image
 import numpy as np
 import h5py
+import cv2
+from matplotlib import pyplot as plt
 
 from keras import backend as K
 K.set_image_data_format('channels_last')
 
 #Load in the pretrained model without the top layer
 model_location ="./Other Files/VGG_Face_pretrained_model_no_top.h5"
-pretrained_model = keras.models.load_model(model_location)
 
 #Set training and validation directory locations
 train_dir="./Dataset/train"
@@ -28,96 +29,71 @@ val_dir="./Dataset/validation"
 
 #Number of training and validation images. Shoule be 5:1 ratio
 nTrain = 20
-nVal = 5
-
-datagen = ImageDataGenerator(rescale=1./255)
+nValidation = 5
+img_width, image_height = 224, 224
 batch_size = 20
 
-#Load training images and generate batches of images and labels
-train_features = np.zeros(shape=(nTrain, 7, 7, 512))
-train_labels = np.zeros(shape=(nTrain,3))
+def save_bottleneck_features():
+    datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = datagen.flow_from_directory(
-    train_dir,
-    target_size = (224,224),
-    batch_size = batch_size,
-    class_mode = 'categorical',
-    shuffle = True
-)
+    pretrained_model = keras.models.load_model(model_location)
+    pretrained_model.summary()
+    generator = datagen.flow_from_directory(
+        train_dir,
+        target_size = (img_width, image_height),
+        batch_size = batch_size,
+        class_mode = None,
+        shuffle = False)
+    bottleneck_features_train = pretrained_model.predict_generator(
+        generator, nTrain // batch_size)
+    np.save(open('bottleneck_features_train.npy', 'wb'),
+        bottleneck_features_train)
 
-#Load validation images and generate batches of images and labels
-val_features = np.zeros(shape=(nVal, 7, 7, 512))
-val_labels = np.zeros(shape=(nVal,3))
+    
+    generator = datagen.flow_from_directory(
+        val_dir,
+        target_size = (img_width, image_height),
+        batch_size = batch_size,
+        class_mode = None,
+        shuffle = False)
+    bottleneck_features_validation = pretrained_model.predict_generator(
+        generator, nValidation // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'wb'),
+        bottleneck_features_validation)
 
-val_generator = datagen.flow_from_directory(
-    val_dir,
-    target_size = (224,224),
-    batch_size = batch_size,
-    class_mode = 'categorical',
-    shuffle = True
-)
 
+def train_top_model():
+    train_data = np.load(open('bottleneck_features_train.npy', 'rb'))
+    train_labels = np.array(
+        [0] * (nTrain // 2) + [1] * (nTrain // 2))
 
-i = 0
-for inputs_batch, labels_batch in train_generator:
-    features_batch = pretrained_model.predict(inputs_batch)
-    train_features[i * batch_size : (i + 1) * batch_size] = features_batch
-    train_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
-    i += 1
-    if i * batch_size >= nTrain:
-        break 
+    validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
+    validation_labels = np.array(
+        [0] * (nValidation // 2) + [1] * (nValidation // 2))
 
-train_features = np.reshape(train_features, (nTrain, 7 * 7 * 512))
+    newModel = keras.models.Sequential()
 
-i = 0
-for inputs_batch, labels_batch in val_generator:
-    features_batch = pretrained_model.predict(inputs_batch)
-    val_features[i * batch_size : (i + 1) * batch_size] = features_batch
-    val_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
-    i += 1
-    if i * batch_size >= nVal:
-        break 
-        
-val_features = np.reshape(val_features, (nVal, 7 * 7 * 512))
+    print("Train Data shape: " + str(train_data.shape[1:]))
 
-newModel = keras.models.Sequential()
-newModel.add(keras.layers.Dense(256, activation = 'relu', input_dim = 7 * 7 * 512))
-newModel.add(keras.layers.Dropout(0.5))
-newModel.add(keras.layers.Dense(3, activation = 'softmax'))
+    layer = Flatten(input_shape=train_data.shape[1:])
 
-newModel.compile(optimizer = optimizers.RMSprop(lr=2e-4),
-                 loss = 'categorical_crossentropy',
-                 metrics =['acc'])
+    newModel.add(layer)
+    newModel.add(keras.layers.Dense(256, activation = 'relu'))
+    newModel.add(keras.layers.Dropout(0.5))
+    newModel.add(keras.layers.Dense(1, activation = 'softmax'))
 
-history = newModel.fit(train_features,
-                       train_labels,
-                       epochs = 20,
-                       batch_size = batch_size,
-                       validation_data = (val_features, val_labels))
+    print("Flatten Layer: " + str(layer.input))
+    newModel.summary()
 
-fnames = val_generator.filenames
+    newModel.compile(optimizer = optimizers.RMSprop(lr=2e-4),
+                loss = 'binary_crossentropy',
+                metrics =['accuracy'])
 
-ground_truth = val_generator.classes
-
-label2index = val_generator.class_indices
-
-index2label = dict((v,k) for k,v in label2index.items())
-
-predictions = newModel.predict_classes(val_features)
-prob = newModel.predict(val_features)
-
-errors = np.where(predictions != ground_truth)[0]
-print('Number of errors = {}/{}'.format(len(errors), nVal))
-
-for i in range (len(errors)):
-    pred_class = np.argmax(prob[errors[i]])
-    pred_label = index2label[pred_class]
-
-    print("Original label: {}, Prediction: {}, Confidence: {:.3f}".format(
-        fnames[errors[i]].split('/')[0],
-        pred_label,
-        prob[errors[i]][pred_class]))
-
-    original = load_img('{}/{}'.format(val_dir, fnames[errors[i]]))
-    plt.imshow(original)
-    plt.show()
+    newModel.fit(train_data,
+                train_labels,
+                epochs = 20,
+                batch_size = batch_size,
+                validation_data = (validation_data, validation_labels))
+    
+save_bottleneck_features()
+train_top_model()
